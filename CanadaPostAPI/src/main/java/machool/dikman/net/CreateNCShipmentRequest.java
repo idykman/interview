f2e9dd25f774649a2e3575660e28d23261f0f1dd
idykman <github@dikman.net>
2017-10-12 12:31:29 -0700

    isolating test code, adding parsing of response

--- CanadaPostAPI/src/main/java/machool/dikman/net/CreateNCShipmentRequest.java
+++ CanadaPostAPI/src/main/java/machool/dikman/net/CreateNCShipmentRequest.java
@@ -1,6 +1,8 @@
 package machool.dikman.net;
 
-import ca.canadapost.ws.ncshipment_v4.*;
+import ca.canadapost.ws.messages.Messages;
+import ca.canadapost.ws.ncshipment_v4.NonContractShipment;
+import ca.canadapost.ws.ncshipment_v4.NonContractShipmentInfo;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.UniformInterfaceException;
@@ -8,7 +10,8 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
-import java.math.BigDecimal;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 public class CreateNCShipmentRequest {
     static class Params{
@@ -39,90 +42,36 @@ public class CreateNCShipmentRequest {
     }
 
 
-    public ClientResponse send() throws UniformInterfaceException {
+    public NonContractShipmentInfo send() throws UniformInterfaceException {
         WebResource aWebResource = apiClient.resource(formUrl());
-        return aWebResource.accept("application/vnd.cpc.ncshipment-v4+xml")
+        return parseResponse(
+                aWebResource.accept("application/vnd.cpc.ncshipment-v4+xml")
                 .header("Content-Type", "application/vnd.cpc.ncshipment-v4+xml")
-                .acceptLanguage("en-CA").post(ClientResponse.class, (Object)params.shipment);
+                .acceptLanguage("en-CA").post(ClientResponse.class, (Object)params.shipment)
+        );
+
     }
 
-    public static void main(String [] args){
+    private NonContractShipmentInfo parseResponse(ClientResponse resp){
+        Object result;
+        try {
+            JAXBContext jc = JAXBContext.newInstance(NonContractShipmentInfo.class, Messages.class);
+            result = jc.createUnmarshaller().unmarshal(resp.getEntityInputStream());
 
-        CreateNCShipmentRequest.Params params = new Params();
-        params.apiUsername = "3fef045f175f8fa3";
-        params.apiPassword = "01457d1244a5498ccb4e6f";
-        params.customerNo = "0008627679";
-        params.shipment = new NonContractShipment();
-        DomesticAddressDetailsType senderAddress = new DomesticAddressDetailsType();
-        senderAddress.setAddressLine1("3838 Albert St");
-        senderAddress.setCity("Burnaby");
-        senderAddress.setProvState("BC");
-        senderAddress.setPostalZipCode("V5C2C9");
+        }catch (JAXBException e){
+            System.out.println(e);
+            return null;
+        }
 
-        SenderType sender = new SenderType();
-        sender.setName("Igor");
-        sender.setCompany("private");
-        sender.setContactPhone("1 (234) 567 8910");
-        sender.setAddressDetails(senderAddress);
+        if( result instanceof NonContractShipmentInfo)
+            return (NonContractShipmentInfo) result;
+        else{
+            for(Messages.Message message : ((Messages) result).getMessages()){
+                System.out.println("Error Code: " + message.getCode() + " Msg: " + message.getDescription());
+            }
+            return null;
+        }
 
-        DestinationAddressDetailsType destinationAddress = new DestinationAddressDetailsType();
-        destinationAddress.setAddressLine1("110 - 328 W Hastings St.");
-        destinationAddress.setCity("Vancouver");
-        destinationAddress.setProvState("BC");
-        destinationAddress.setCountryCode("CA");
-        destinationAddress.setPostalZipCode("V6B 1K6");
-
-        DestinationType destination = new DestinationType();
-        destination.setName("Vincent Tellier");
-        destination.setCompany("Machool.");
-        destination.setAddressDetails(destinationAddress);
-
-        OptionType option1 = new OptionType();
-        option1.setOptionCode("DC");
-        DeliverySpecType.Options options = new DeliverySpecType.Options();
-        options.getOptions().add(option1);
-
-        ParcelCharacteristicsType.Dimensions parcelDimension = new ParcelCharacteristicsType.Dimensions();
-        parcelDimension.setHeight(new BigDecimal(6));
-        parcelDimension.setLength(new BigDecimal(12));
-        parcelDimension.setWidth(new BigDecimal(9));
-
-        ParcelCharacteristicsType characteristics = new ParcelCharacteristicsType();
-        characteristics.setWeight(new BigDecimal(5));
-        characteristics.setDimensions(parcelDimension);
-
-        NotificationType notification = new NotificationType();;
-        notification.setEmail("machool@dikman.net");
-        notification.setOnDelivery(true);
-        notification.setOnException(false);
-        notification.setOnShipment(true);
-
-        PreferencesType preferences = new PreferencesType();
-        preferences.setShowInsuredValue(true);
-        preferences.setShowPackingInstructions(false);
-        preferences.setShowPostageRate(true);
-
-        ReferencesType references = new ReferencesType();
-        references.setCostCentre("costCentre");
-        references.setCustomerRef1("custRef1");
-        references.setCustomerRef2("custRef2");
-
-        DeliverySpecType deliverySpec = new DeliverySpecType();
-        deliverySpec.setServiceCode("DOM.EP");
-        deliverySpec.setSender(sender);
-        deliverySpec.setDestination(destination);
-        deliverySpec.setOptions(options);
-        deliverySpec.setParcelCharacteristics(characteristics);
-        deliverySpec.setNotification(notification);
-        deliverySpec.setPreferences(preferences);
-        deliverySpec.setReferences(references);
-
-        params.shipment.setRequestedShippingPoint("K1K1K1");
-        params.shipment.setDeliverySpec(deliverySpec);
-
-        CreateNCShipmentRequest request = new CreateNCShipmentRequest(params);
-        ClientResponse response = request.send();
-
-        System.out.println("test");
     }
+
 }
---
+++ CanadaPostAPI/src/test/java/machool/dikman/net/CreateNCShipmentRequestTest.java
@@ -0,0 +1,104 @@
+package machool.dikman.net;
+
+import ca.canadapost.ws.ncshipment_v4.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+
+public class CreateNCShipmentRequestTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSend() throws Exception {
+        CreateNCShipmentRequest.Params params = new CreateNCShipmentRequest.Params();
+        params.apiUsername = "3fef045f175f8fa3";
+        params.apiPassword = "01457d1244a5498ccb4e6f";
+        params.customerNo = "0008627679";
+        params.shipment = createTestNCShipment();
+
+        CreateNCShipmentRequest request = new CreateNCShipmentRequest(params);
+        NonContractShipmentInfo info = request.send();
+
+        System.out.println("Result:" + info);
+    }
+
+    private static NonContractShipment createTestNCShipment(){
+        DomesticAddressDetailsType senderAddress = new DomesticAddressDetailsType();
+        senderAddress.setAddressLine1("3838 Albert St");
+        senderAddress.setCity("Burnaby");
+        senderAddress.setProvState("BC");
+        senderAddress.setPostalZipCode("V5C2C9");
+
+        SenderType sender = new SenderType();
+        sender.setName("Igor");
+        sender.setCompany("private");
+        sender.setContactPhone("1 (234) 567 8910");
+        sender.setAddressDetails(senderAddress);
+
+        DestinationAddressDetailsType destinationAddress = new DestinationAddressDetailsType();
+        destinationAddress.setAddressLine1("110 - 328 W Hastings St.");
+        destinationAddress.setCity("Vancouver");
+        destinationAddress.setProvState("BC");
+        destinationAddress.setCountryCode("CA");
+        destinationAddress.setPostalZipCode("V6B 1K6");
+
+        DestinationType destination = new DestinationType();
+        destination.setName("Vincent Tellier");
+        destination.setCompany("Machool.");
+        destination.setAddressDetails(destinationAddress);
+
+        OptionType option1 = new OptionType();
+        option1.setOptionCode("DC");
+        DeliverySpecType.Options options = new DeliverySpecType.Options();
+        options.getOptions().add(option1);
+
+        ParcelCharacteristicsType.Dimensions parcelDimension = new ParcelCharacteristicsType.Dimensions();
+        parcelDimension.setHeight(new BigDecimal(6));
+        parcelDimension.setLength(new BigDecimal(12));
+        parcelDimension.setWidth(new BigDecimal(9));
+
+        ParcelCharacteristicsType characteristics = new ParcelCharacteristicsType();
+        characteristics.setWeight(new BigDecimal(5));
+        characteristics.setDimensions(parcelDimension);
+
+        NotificationType notification = new NotificationType();;
+        notification.setEmail("machool@dikman.net");
+        notification.setOnDelivery(true);
+        notification.setOnException(false);
+        notification.setOnShipment(true);
+
+        PreferencesType preferences = new PreferencesType();
+        preferences.setShowInsuredValue(true);
+        preferences.setShowPackingInstructions(false);
+        preferences.setShowPostageRate(true);
+
+        ReferencesType references = new ReferencesType();
+        references.setCostCentre("costCentre");
+        references.setCustomerRef1("custRef1");
+        references.setCustomerRef2("custRef2");
+
+        DeliverySpecType deliverySpec = new DeliverySpecType();
+        deliverySpec.setServiceCode("DOM.EP");
+        deliverySpec.setSender(sender);
+        deliverySpec.setDestination(destination);
+        deliverySpec.setOptions(options);
+        deliverySpec.setParcelCharacteristics(characteristics);
+        deliverySpec.setNotification(notification);
+        deliverySpec.setPreferences(preferences);
+        deliverySpec.setReferences(references);
+        NonContractShipment result = new NonContractShipment();
+        result.setRequestedShippingPoint("K1K1K1");
+        result.setDeliverySpec(deliverySpec);
+        return result;
+    }
+
+}
